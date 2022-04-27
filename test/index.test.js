3bd45da7b4899bd8607854c7d429ff1ae64ec3e8
Durran Jordan <durran@gmail.com>
2022-04-27 16:12:17 +0200

    test: update unit test

--- test/index.test.js
+++ test/index.test.js
@@ -7,16 +7,7 @@ describe('zstd', () => {
 
     it('returns a compressed buffer', async () => {
       const result = await compress(buffer);
-      expect(result).to.deep.equal(buffer);
-    });
-  });
-
-  describe('#decompress', () => {
-    const buffer = Buffer.from('test');
-
-    it('returns the buffer', async () => {
-      const result = await decompress(buffer);
-      expect(result).to.deep.equal(buffer);
+      expect(await decompress(result)).to.deep.equal(buffer);
     });
   });
 });
