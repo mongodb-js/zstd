d30d09bf1cc988c31bb77bfee20f5b6e772c06a4
Durran Jordan <durran@gmail.com>
2022-05-17 15:32:28 +0200

    chore(NODE-4250): simplify load code

--- index.js
+++ index.js
@@ -7,51 +7,7 @@ let nativeBinding = null
 let localFileExisted = false
 let loadError = null
 
-function isMusl() {
-  // For Node 10
-  if (!process.report || typeof process.report.getReport !== 'function') {
-    try {
-      return readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
-    } catch (e) {
-      return true
-    }
-  } else {
-    const { glibcVersionRuntime } = process.report.getReport().header
-    return !glibcVersionRuntime
-  }
-}
-
 switch (platform) {
-  case 'android':
-    switch (arch) {
-      case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'zstd.android-arm64.node'))
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./zstd.android-arm64.node')
-          } else {
-            nativeBinding = require('@mongodb-js/zstd-android-arm64')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'zstd.android-arm-eabi.node'))
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./zstd.android-arm-eabi.node')
-          } else {
-            nativeBinding = require('@mongodb-js/zstd-android-arm-eabi')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      default:
-        throw new Error(`Unsupported architecture on Android ${arch}`)
-    }
-    break
   case 'win32':
     switch (arch) {
       case 'x64':
@@ -63,20 +19,6 @@ switch (platform) {
             nativeBinding = require('./zstd.win32-x64-msvc.node')
           } else {
             nativeBinding = require('@mongodb-js/zstd-win32-x64-msvc')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      case 'ia32':
-        localFileExisted = existsSync(
-          join(__dirname, 'zstd.win32-ia32-msvc.node')
-        )
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./zstd.win32-ia32-msvc.node')
-          } else {
-            nativeBinding = require('@mongodb-js/zstd-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -150,61 +92,31 @@ switch (platform) {
   case 'linux':
     switch (arch) {
       case 'x64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'zstd.linux-x64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./zstd.linux-x64-musl.node')
-            } else {
-              nativeBinding = require('@mongodb-js/zstd-linux-x64-musl')
-            }
-          } catch (e) {
-            loadError = e
+        localFileExisted = existsSync(
+          join(__dirname, 'zstd.linux-x64-gnu.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./zstd.linux-x64-gnu.node')
+          } else {
+            nativeBinding = require('@mongodb-js/zstd-linux-x64-gnu')
           }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'zstd.linux-x64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./zstd.linux-x64-gnu.node')
-            } else {
-              nativeBinding = require('@mongodb-js/zstd-linux-x64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
+        } catch (e) {
+          loadError = e
         }
         break
       case 'arm64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'zstd.linux-arm64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./zstd.linux-arm64-musl.node')
-            } else {
-              nativeBinding = require('@mongodb-js/zstd-linux-arm64-musl')
-            }
-          } catch (e) {
-            loadError = e
+        localFileExisted = existsSync(
+          join(__dirname, 'zstd.linux-arm64-gnu.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./zstd.linux-arm64-gnu.node')
+          } else {
+            nativeBinding = require('@mongodb-js/zstd-linux-arm64-gnu')
           }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'zstd.linux-arm64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./zstd.linux-arm64-gnu.node')
-            } else {
-              nativeBinding = require('@mongodb-js/zstd-linux-arm64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
+        } catch (e) {
+          loadError = e
         }
         break
       case 'arm':
