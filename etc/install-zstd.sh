a5bc5cd952bb523cff32dfec0dcf76d216000fc6
bailey <bailey.pearson@mongodb.com>
2025-02-05 07:54:04 -0700

    saved

--- etc/install-zstd.sh
+++ etc/install-zstd.sh
@@ -8,18 +8,20 @@ clean_deps() {
 
 download_zstd() {
 	mkdir -p deps/zstd
+
+	echo "NODE VERSION: $(node --version)"
 	ZSTD_VERSION=$(node -p "require('./package.json')['mongodb:zstd_version']")
 
 	# only unpack the source and build files needed to compile the project
 	necessary_files="zstd-$ZSTD_VERSION/build zstd-$ZSTD_VERSION/lib zstd-$ZSTD_VERSION/programs"
-	
+
 	# flags
 	# -L                       follow redirects
 	# -C                       output directory
 	# -                        tar from stdin
 	# --strip-components       ignore the top-level directory when unpacking
-	curl -L "https://github.com/facebook/zstd/releases/download/v$ZSTD_VERSION/zstd-$ZSTD_VERSION.tar.gz" \
-	 	| tar  -zxf - -C deps/zstd --strip-components 1 $necessary_files
+	curl -L "https://github.com/facebook/zstd/releases/download/v$ZSTD_VERSION/zstd-$ZSTD_VERSION.tar.gz" |
+		tar -zxf - -C deps/zstd --strip-components 1 $necessary_files
 }
 
 build_zstd() {
@@ -37,9 +39,9 @@ build_zstd() {
 		-DZSTD_BUILD_SHARED=OFF \
 		-DCMAKE_OSX_ARCHITECTURES='x86_64;arm64' \
 		-DCMAKE_BUILD_TYPE=Release \
-	    ../build/cmake 
+		../build/cmake
 
-	cmake --build .  --target libzstd_static --config Release
+	cmake --build . --target libzstd_static --config Release
 }
 
 clean_deps
